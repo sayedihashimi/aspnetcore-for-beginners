f304ff8618845922d75e74d05bb892772243bc37
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2019-09-15 13:49:08 -0400

    adding js filter to title

--- Final_Project/RazorPagesMovie/wwwroot/js/site.js
+++ Final_Project/RazorPagesMovie/wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿// Write your Javascript code.
+﻿$("input[name='SearchString']").on('input', function () {
+    ShowHideTableEntries($("input[name='SearchString']").val());
+});
+
+function ShowHideTableEntries(searchText) {
+    // make all visible and return
+    if (!searchText || searchText.length <= 0) {
+        $(".table>tbody>tr").css('display','');
+        return;
+    }
+
+    // for each entry in table hide any entry that doesn't contain the text
+    $(".table>tbody>tr>td:first-child").each(function (index) {
+        var elementText = $(this).text();
+        // if it matches search make it visible otherwise hidden
+        if (elementText.includes(searchText)) {
+            $(this).parent().css('display','');
+        }
+        else {
+            $(this).parent().css('display','none');
+        }
+    });
+}
